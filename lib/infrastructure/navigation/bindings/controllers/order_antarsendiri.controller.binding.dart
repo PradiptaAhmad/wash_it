a82fde090cc8f7a1946f83b68218c753fa218372
Marlen Satriani <marlen7891011@gmail.com>
2024-06-04 07:25:25 +0700

    [MS] Controller Fix

--- lib/infrastructure/navigation/bindings/controllers/order_antarsendiri.controller.binding.dart
+++ lib/infrastructure/navigation/bindings/controllers/order_antarsendiri.controller.binding.dart
@@ -1,11 +1,12 @@
 import 'package:get/get.dart';
 import 'package:wash_it/infrastructure/navigation/bindings/controllers/order_controller.dart';
+import 'package:wash_it/presentation/order_antarsendiri/controllers/order_antarjemput.controller.dart';
 
 class OrderAntarSendiriPageControllerBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<OrderController>(
-          () => OrderController(),
+    Get.lazyPut<OrderAntarSendiriController>(
+      () => OrderAntarSendiriController(),
     );
   }
 }
